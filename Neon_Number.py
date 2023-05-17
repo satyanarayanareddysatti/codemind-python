de69a836c93d176f3262fc50d55680e231682bf0
satyanarayanareddysatti <119919492+satyanarayanareddysatti@users.noreply.github.com>
2023-05-17 20:43:51 +0530

    def neon(sq):     sum_digits = 0     num_str=str(sq)     for digit in num_str:         digit_val = int(digit)         sum_digits += digit_val     return sum_digits num = int(input()) sq=num*num res=neon(sq) if num==res:     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,14 +1,14 @@
 def neon(sq):
     sum_digits = 0
     num_str=str(sq)
-    for digit in num_str:
+    for digit in num_str: 
         digit_val = int(digit)
         sum_digits += digit_val
     return sum_digits
 num = int(input())
 sq=num*num
 res=neon(sq)
-if num==res:
+if num==res: 
     print("Neon Number")
-else:
+else: 
     print("Not Neon Number")
