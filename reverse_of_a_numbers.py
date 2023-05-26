d597898bd8df8639ba67379db848d2d05509974b
satyanarayanareddysatti <119919492+satyanarayanareddysatti@users.noreply.github.com>
2023-05-26 11:28:33 +0530

    n=int(input()) rev=0 while n:     s=n%10     rev=rev*10+s     n=n//10 print(rev)

--- reverse_of_a_numbers.py
+++ reverse_of_a_numbers.py
@@ -1,16 +1,7 @@
-s=input()
-lst=list(s)
-lst.reverse()
-
-s="".join(lst)
-
-if s[-1]=='-':
-    if s[0]=='0':
-        print('-' + s[1:-1])
-    else:
-        print('-' + s[:-1])
-else:
-    if s[0]=='0':
-        print(s[1:])
-    else:
-        print(s)
+n=int(input())
+rev=0
+while n:
+    s=n%10
+    rev=rev*10+s
+    n=n//10
+print(rev)
